d84c7bb0e784459108adbf8debc65058a92d3d67
Wishlove09 <1176293852@qq.com>
2023-11-07 08:52:16 +0800

    init

---
+++ src/main/java/edu/xmut/examsys/web/BaseServlet.java
@@ -0,0 +1,19 @@
+package edu.xmut.examsys.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author 朔风
+ * @date 2023-11-07 08:50
+ */
+public class BaseServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
