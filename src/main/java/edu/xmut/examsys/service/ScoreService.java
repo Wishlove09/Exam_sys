069d2196171b2bcc235a8ae796e6ceb13c98a19c
Wishlove09 <1176293852@qq.com>
2023-11-13 23:15:09 +0800

    分数接口

---
+++ src/main/java/edu/xmut/examsys/service/ScoreService.java
@@ -0,0 +1,11 @@
+package edu.xmut.examsys.service;
+
+
+/**
+* @author shuofeng
+* @description 针对表【score(成绩表)】的数据库操作Service
+* @createDate 2023-11-13 16:58:53
+*/
+public interface ScoreService  {
+
+}
