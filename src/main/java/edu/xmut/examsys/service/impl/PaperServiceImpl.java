aeabe277dbbd740c2995763d67b8c88dfb6f1e47
Wishlove09 <1176293852@qq.com>
2023-11-13 23:15:25 +0800

    试卷接口

---
+++ src/main/java/edu/xmut/examsys/service/impl/PaperServiceImpl.java
@@ -0,0 +1,19 @@
+package edu.xmut.examsys.service.impl;
+
+import edu.xmut.examsys.service.PaperService;
+
+import fun.shuofeng.myspringmvc.annotaion.Service;
+
+/**
+* @author shuofeng
+* @description 针对表【paper(试卷表)】的数据库操作Service实现
+* @createDate 2023-11-13 16:59:00
+*/
+@Service
+public class PaperServiceImpl    implements PaperService{
+
+}
+
+
+
+
