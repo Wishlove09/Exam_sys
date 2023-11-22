9c9c3aed30788c3cdf5165f93581cd9fdd3e6373
Wishlove09 <1176293852@qq.com>
2023-11-22 10:21:44 +0800

    班级vo

---
+++ src/main/java/edu/xmut/examsys/bean/vo/ClazzVO.java
@@ -0,0 +1,22 @@
+package edu.xmut.examsys.bean.vo;
+
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * @author 朔风
+ * @date 2023-11-17 14:43
+ */
+@Data
+@Builder
+public class ClazzVO {
+    /**
+     * 班级id
+     */
+    private Long classId;
+
+    /**
+     * 班级名称
+     */
+    private String className;
+}
