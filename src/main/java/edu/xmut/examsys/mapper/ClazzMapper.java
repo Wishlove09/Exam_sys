04fa8cb559e441e5f21f88a34bde54fce33b3369
Wishlove09 <1176293852@qq.com>
2023-11-27 21:08:54 +0800

    班级CURED

---
+++ src/main/java/edu/xmut/examsys/mapper/ClazzMapper.java
@@ -0,0 +1,18 @@
+package edu.xmut.examsys.mapper;
+
+import edu.xmut.examsys.bean.Clazz;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author shuofeng
+* @description 针对表【clazz(班级表)】的数据库操作Mapper
+* @createDate 2023-11-27 21:08:15
+* @Entity edu.xmut.examsys.bean.Clazz
+*/
+public interface ClazzMapper extends BaseMapper<Clazz> {
+
+}
+
+
+
+
