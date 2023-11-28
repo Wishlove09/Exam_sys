6cbaf337281442423da0dd45918ae998dd0466f0
Wishlove09 <1176293852@qq.com>
2023-11-28 16:15:42 +0800

    班级VO 新增院系字段

--- src/main/java/edu/xmut/examsys/bean/vo/ClazzVO.java
+++ src/main/java/edu/xmut/examsys/bean/vo/ClazzVO.java
@@ -1,7 +1,9 @@
 package edu.xmut.examsys.bean.vo;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author 朔风
@@ -9,6 +11,8 @@ import lombok.Data;
  */
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ClazzVO {
     /**
      * 班级id
