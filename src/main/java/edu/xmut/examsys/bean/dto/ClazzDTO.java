5e04b95c0fb04154cf61bc9539a883b5c4467e09
Wishlove09 <1176293852@qq.com>
2023-11-28 14:25:27 +0800

    班级表单类

---
+++ src/main/java/edu/xmut/examsys/bean/dto/ClazzDTO.java
@@ -0,0 +1,18 @@
+package edu.xmut.examsys.bean.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author 朔风
+ * @date 2023-11-28 11:36
+ */
+@NoArgsConstructor
+@Data
+@AllArgsConstructor
+public class ClazzDTO {
+
+    private String className;
+    private Long classId;
+}
