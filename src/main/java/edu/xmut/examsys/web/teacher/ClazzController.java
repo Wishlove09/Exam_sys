627c82e2fdd172eb659625e785b305bc24ccda67
Wishlove09 <1176293852@qq.com>
2023-11-28 14:25:42 +0800

    新增班级添加接口

--- src/main/java/edu/xmut/examsys/web/teacher/ClazzController.java
+++ src/main/java/edu/xmut/examsys/web/teacher/ClazzController.java
@@ -1,13 +1,19 @@
 package edu.xmut.examsys.web.teacher;
 
+import com.alibaba.fastjson2.JSONObject;
 import edu.xmut.examsys.bean.Clazz;
+import edu.xmut.examsys.bean.dto.ClazzDTO;
+import edu.xmut.examsys.exception.GlobalException;
 import edu.xmut.examsys.service.ClazzService;
 import edu.xmut.examsys.utils.R;
 import fun.shuofeng.myspringmvc.annotaion.Autowired;
 import fun.shuofeng.myspringmvc.annotaion.Controller;
 import fun.shuofeng.myspringmvc.annotaion.RequestMapping;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
+
+import static edu.xmut.examsys.constants.SystemConstant.MISSING_ARGUMENT;
 
 /**
  * @author 朔风
@@ -21,8 +27,19 @@ public class ClazzController {
     ClazzService clazzService;
 
     @RequestMapping("/getAll")
-    public R getALl() {
+    public R getAll() {
         List<Clazz> list = clazzService.getAll();
         return R.ok(list);
+    }
+
+    @RequestMapping(value = "/add", method = "post")
+    public R add(String json) {
+        if (StringUtils.isBlank(json)) {
+            throw new GlobalException(MISSING_ARGUMENT);
+        }
+        ClazzDTO clazzDTO = JSONObject.parseObject(json, ClazzDTO.class);
+        Boolean result = clazzService.add(clazzDTO);
+
+        return result ? R.ok() : R.fail();
     }
 }
