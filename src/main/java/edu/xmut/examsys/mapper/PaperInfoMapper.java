85333a12b4b7b4b2b52d17fc4b54b7b1569b3c10
Wishlove09 <1176293852@qq.com>
2023-11-30 11:15:04 +0800

    试卷CURD

---
+++ src/main/java/edu/xmut/examsys/mapper/PaperInfoMapper.java
@@ -0,0 +1,15 @@
+package edu.xmut.examsys.mapper;
+
+/**
+* @author shuofeng
+* @description 针对表【paper_info(试卷信息表)】的数据库操作Mapper
+* @createDate 2023-11-30 10:44:34
+* @Entity edu.xmut.examsys.bean.PaperInfo
+*/
+public interface PaperInfoMapper {
+
+}
+
+
+
+
