9817e700e108699b0aeb8f7fd62231d807b6cb93
Wishlove09 <1176293852@qq.com>
2023-11-30 08:34:59 +0800

    多选题表单类

---
+++ src/main/java/edu/xmut/examsys/bean/dto/MultiQuestionDTO.java
@@ -0,0 +1,21 @@
+package edu.xmut.examsys.bean.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * @author 朔风
+ * @date 2023-11-30 08:31
+ */
+@NoArgsConstructor
+@Data
+public class MultiQuestionDTO {
+
+    private List<String> rightAnswer;
+    private String content;
+    private String analysis;
+    private List<OptionDTO> optionList;
+
+}
