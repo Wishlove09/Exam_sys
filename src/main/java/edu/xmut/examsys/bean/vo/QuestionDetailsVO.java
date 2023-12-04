7463e1833bfb063526d5fbfe9fcc1afcfdf1743f
Wishlove09 <1176293852@qq.com>
2023-12-04 20:58:42 +0800

    问题选项vo

---
+++ src/main/java/edu/xmut/examsys/bean/vo/QuestionDetailsVO.java
@@ -0,0 +1,31 @@
+package edu.xmut.examsys.bean.vo;
+
+import edu.xmut.examsys.bean.dto.OptionDTO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * @author 朔风
+ * @date 2023-11-22 10:38
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class QuestionDetailsVO {
+
+    private String id;
+    private String content;
+    private Integer type;
+    private Integer level;
+    private String rightAnswer;
+    private String image;
+
+    private List<OptionVO> optionList;
+
+
+}
