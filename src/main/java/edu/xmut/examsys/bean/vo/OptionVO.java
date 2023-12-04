f4733ac3d28b2f6ba9ae7bc69d0765f47e3a984b
Wishlove09 <1176293852@qq.com>
2023-12-04 20:58:32 +0800

    选项vo

---
+++ src/main/java/edu/xmut/examsys/bean/vo/OptionVO.java
@@ -0,0 +1,14 @@
+package edu.xmut.examsys.bean.vo;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class OptionVO {
+
+    private String id;
+    private String title;
+    private String content;
+    private String image;
+}
