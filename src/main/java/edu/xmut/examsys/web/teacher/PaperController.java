e1c72e4bdacc012fc353a8428b3f65405530ee63
Wishlove09 <1176293852@qq.com>
2023-12-05 15:09:25 +0800

    试卷接口 新增试卷分页

--- src/main/java/edu/xmut/examsys/web/teacher/PaperController.java
+++ src/main/java/edu/xmut/examsys/web/teacher/PaperController.java
@@ -1,7 +1,13 @@
 package edu.xmut.examsys.web.teacher;
 
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
+import edu.xmut.examsys.bean.dto.PageDTO;
+import edu.xmut.examsys.bean.vo.PageVO;
 import edu.xmut.examsys.exception.GlobalException;
+import edu.xmut.examsys.service.PaperService;
 import edu.xmut.examsys.utils.R;
+import fun.shuofeng.myspringmvc.annotaion.Autowired;
 import fun.shuofeng.myspringmvc.annotaion.Controller;
 import fun.shuofeng.myspringmvc.annotaion.RequestMapping;
 import org.apache.commons.lang3.StringUtils;
@@ -16,15 +22,18 @@ import static edu.xmut.examsys.constants.SystemConstant.MISSING_ARGUMENT;
 @RequestMapping("/paper")
 public class PaperController {
 
+    @Autowired
+    PaperService paperService;
+
 
     @RequestMapping(value = "/pages", method = "post")
     public R pages(String json) {
         if (StringUtils.isBlank(json)) {
             throw new GlobalException(MISSING_ARGUMENT);
         }
+        PageDTO pageDTO = JSONObject.parseObject(json, PageDTO.class);
+        PageVO pageVO = paperService.pages(pageDTO);
 
-
-
-        return R.ok();
+        return R.ok(pageVO);
     }
 }
