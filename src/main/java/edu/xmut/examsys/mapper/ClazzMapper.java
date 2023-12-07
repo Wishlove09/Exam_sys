925dcc2aab9f236237088cc18f6c9657c3555e0f
Wishlove09 <1176293852@qq.com>
2023-12-07 19:07:55 +0800

    班级CURD

--- src/main/java/edu/xmut/examsys/mapper/ClazzMapper.java
+++ src/main/java/edu/xmut/examsys/mapper/ClazzMapper.java
@@ -15,7 +15,7 @@ import java.util.List;
  */
 public interface ClazzMapper {
 
-    ClazzStudent selectCStById(Long userId);
+    List<ClazzStudent> selectCStById(Long userId);
 
     Clazz selectClazzById(Long clazzId);
 
