5c0a94a8ce1b000a770fa0aab89db0ce7ed72df3
Wishlove09 <1176293852@qq.com>
2023-12-07 19:05:07 +0800

    考试信息

--- src/main/java/edu/xmut/examsys/mapper/ExamInfoMapper.java
+++ src/main/java/edu/xmut/examsys/mapper/ExamInfoMapper.java
@@ -2,17 +2,27 @@ package edu.xmut.examsys.mapper;
 
 import com.github.pagehelper.Page;
 import edu.xmut.examsys.bean.ExamInfo;
-import edu.xmut.examsys.bean.dto.PageDTO;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
-* @author shuofeng
-* @description 针对表【exam_info(考试信息表)】的数据库操作Mapper
-* @createDate 2023-12-06 21:23:01
-* @Entity edu.xmut.examsys.bean.ExamInfo
-*/
+ * @author shuofeng
+ * @description 针对表【exam_info(考试信息表)】的数据库操作Mapper
+ * @createDate 2023-12-06 21:23:01
+ * @Entity edu.xmut.examsys.bean.ExamInfo
+ */
 public interface ExamInfoMapper {
 
     Page<ExamInfo> pages();
+
+    Integer insert(ExamInfo examInfo);
+
+    Page<ExamInfo> selectById(
+            @Param("examIds")
+            List<Long> examIds,
+            @Param("search")
+            String search);
 }
 
 
