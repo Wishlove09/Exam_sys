bc74ebc2b7f7ded5bad91a7614a06439a15ae86a
Wishlove09 <1176293852@qq.com>
2023-12-09 23:37:30 +0800

    考试管理接口 新增更新接口

--- src/main/java/edu/xmut/examsys/web/teacher/ExamManageController.java
+++ src/main/java/edu/xmut/examsys/web/teacher/ExamManageController.java
@@ -1,9 +1,12 @@
 package edu.xmut.examsys.web.teacher;
 
+import cn.hutool.core.date.DateUtil;
 import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONObject;
 import edu.xmut.examsys.bean.dto.ExamAddDTO;
+import edu.xmut.examsys.bean.dto.ExamInfoDTO;
 import edu.xmut.examsys.bean.dto.PageDTO;
+import edu.xmut.examsys.bean.vo.ExamDetailsVO;
 import edu.xmut.examsys.bean.vo.ExamInfoVO;
 import edu.xmut.examsys.bean.vo.PageVO;
 import edu.xmut.examsys.exception.GlobalException;
@@ -15,6 +18,8 @@ import fun.shuofeng.myspringmvc.annotaion.RequestMapping;
 import org.apache.commons.lang3.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
+
+import java.util.Date;
 
 import static edu.xmut.examsys.constants.SystemConstant.MISSING_ARGUMENT;
 import static edu.xmut.examsys.constants.SystemConstant.USER_ID;
@@ -55,5 +60,35 @@ public class ExamManageController {
         Boolean result = examService.addExam(examAddDTO, request);
 
         return result ? R.ok("添加成功") : R.fail("添加失败");
+    }
+
+
+    @RequestMapping("/details")
+    public R details(HttpServletRequest request) {
+        String id = request.getParameter("examId");
+        if (StringUtils.isBlank(id)) {
+            throw new GlobalException(MISSING_ARGUMENT);
+        }
+        ExamInfoVO examInfoVO = examService.getExamInfoById(id);
+        Date startTime = examInfoVO.getStartTime();
+        Date endTime = examInfoVO.getEndTime();
+        examInfoVO.setExamDate(new String[]{DateUtil.format(startTime, "yyyy-MM-dd HH:mm:ss"),
+                DateUtil.format(endTime, "yyyy-MM-dd HH:mm:ss")});
+
+        return R.ok(examInfoVO);
+    }
+
+
+    @RequestMapping(value = "/update", method = "post")
+    public R update(String json) {
+        if (StringUtils.isBlank(json)) {
+            throw new GlobalException(MISSING_ARGUMENT);
+        }
+        ExamInfoDTO examInfoDTO = JSONObject.parseObject(json, ExamInfoDTO.class);
+
+        Boolean update = examService.update(examInfoDTO);
+
+
+        return update ? R.ok() : R.fail();
     }
 }
