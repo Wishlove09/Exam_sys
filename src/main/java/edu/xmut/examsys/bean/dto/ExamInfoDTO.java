e516a50b1ec0546535ad7580306eb933a87aec96
Wishlove09 <1176293852@qq.com>
2023-12-09 23:35:38 +0800

    考试信息DTO

--- src/main/java/edu/xmut/examsys/bean/dto/ExamInfoDTO.java
+++ src/main/java/edu/xmut/examsys/bean/dto/ExamInfoDTO.java
@@ -1,8 +1,28 @@
 package edu.xmut.examsys.bean.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
 /**
  * @author 朔风
  * @date 2023-11-13 23:48
  */
+@NoArgsConstructor
+@Data
 public class ExamInfoDTO {
+
+
+    private String attention;
+    private String creator;
+    private String desc;
+    private List<String> examDate;
+    private Long examId;
+    private Integer timeLimit;
+    private String title;
+    private Integer totalScore;
+    private Integer totalTime;
+
+
 }
